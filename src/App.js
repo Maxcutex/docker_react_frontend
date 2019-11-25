fd6d876faca62256de3f9c065620601bccd783e3
Eno Bassey <ennyboy@gmail.com>
2019-11-25 14:29:19 +0100

    added travis deploy config

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> 
-          come and see asfafdasdfsdafsdfasfasfasf  asfdsa 
+          come and see asfafdasdfsdafsdfasfasfasf 
         </p>
         <a
           className="App-link"
